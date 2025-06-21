1e60b0e6ff42a16e345e8d987141380f064b08d4
arlyadewi <arlyadewi17@gmail.com>
2025-06-21 14:17:03 +0800

    Add files via upload

---
+++ src/pertemuan10/Pertemuan10.java
@@ -0,0 +1,17 @@
+package pertemuan10;
+
+/**
+ *
+ * @author Arlya Dewi
+ * Tgl 24/05/2025
+ */
+public class Pertemuan10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
